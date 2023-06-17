98094853585613cd1334b678ca92262119eb3f65
Dmitry Olshansky <dmitry@olshansky.me>
2023-06-17 22:21:33 +0300

    Opcodes

--- src/rewind/re/ir.d
+++ src/rewind/re/ir.d
@@ -42,3 +42,32 @@ struct Re {
 
     Matcher engine();
 }
+
+enum Opcode : ubyte {
+    ANY = 0,
+    CHAR,
+    NOTCHAR,
+    CHARCLASS,
+    NOTCHARCLASS,
+    JMP,
+    FORK,
+    COUNTED_LOOP
+}
+
+void put32(ref ubyte[] dest, uint arg) {
+    dest ~= arg & 0xFF;
+    dest ~= (arg >> 8) & 0xFF;
+    dest ~= (arg >> 16) & 0xFF;
+    dest ~= (arg >> 24) & 0xFF;
+}
+
+void encode(Opcode op, T... args)(ref ubyte[] dest, T args) {
+    static if (op == Opcode.ANY) {
+        dest ~= op;
+    } else static if (op == Opcode.CHAR) {
+        dest ~= op;
+        put32(dest, args[0]);
+    } else {
+        static assert(false, "TODO");
+    }
+}
