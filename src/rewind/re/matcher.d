743a50487c5da182a2f9f28a235b2b2f0de523ca
Dmitry Olshansky <dmitry@olshansky.me>
2023-06-21 12:56:10 +0300

    A start on matchers, Empty & Char

--- src/rewind/re/matcher.d
+++ src/rewind/re/matcher.d
@@ -4,10 +4,42 @@ import rewind.re.captures;
 
 interface Matcher {
     bool exact();
-    bool matches(const(char)[] slice);
-    bool hasMatch(const(char)[] slice);
-    const(char)[] locate(const(char)[] slice);
-    Captures fullyMatch(ref const(char)[] slice);
+    bool realMatches(const(char)[] slice);
+    bool realHasMatch(const(char)[] slice);
+    const(char)[] realLocate(const(char)[] slice);
+    Captures realFullyMatch(ref const(char)[] slice);
     Matcher next(); // next matcher in the chain
 }
 
+final class Empty : Matcher {
+    bool exact() { return true; }
+    bool realMatches(const(char)[] slice) { return true; }
+    bool realHasMatch(const(char)[] slice) { return true; }
+    const(char)[] realLocate(const(char)[] slice) { return null; }
+    Captures realFullyMatch(ref const(char)[] slice) {
+        if (slice.length > 0) {
+            const cap = slice[0..1];
+            slice = slice[1..$];
+            return Captures(cap);
+        }
+        return Captures();
+    }
+}
+
+final class Char : Matcher {
+    private char ch;
+    private bool exact;
+    this(char ch, bool exact) {
+        this.ch = ch;
+        this.exact = exact;
+    }
+    bool exact(){ return exact; }
+    bool realMatches(const(char)[] slice) {
+        return slice.length > 0 ? slice[0] == ch : false;
+    }
+    bool realHasMatch(const(char)[] slice) {
+        import core.stdc.string;
+        return memchr(slice.ptr, slice.length, ch) != null;
+    }
+
+}
